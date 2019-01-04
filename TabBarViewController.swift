7b65e8f4b98d208cd1636c6bb78c8cf45f593487
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-01-04 21:00:10 +0200

    start

---
+++ TabBarViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarViewController.swift
+//  TabBarWhithTeam
+//
+//  Created by Артем on 1/4/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
