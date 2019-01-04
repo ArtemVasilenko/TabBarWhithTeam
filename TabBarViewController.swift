49c5b59ed4cf0510c2539abca58d9fe090bfeba5
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-01-04 21:09:21 +0200

    task 3 - Done

--- TabBarViewController.swift
+++ TabBarViewController.swift
@@ -12,8 +12,7 @@ class TabBarViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = .white
     }
     
 
